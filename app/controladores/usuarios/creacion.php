dcf3feb33ebe9b8364d892c21e401e10ef03138f
LeinadOdreiuqzi <sherdanialex159357@gmail.com>
2024-09-30 11:13:06 -0500

    Fix en  la  redirección al crear un usuario

--- app/controladores/usuarios/creacion.php
+++ app/controladores/usuarios/creacion.php
@@ -5,7 +5,7 @@ $nombres = $_POST['nombres'];
 $email = $_POST['email'];
 $clave_usuario = $_POST['clave_usuario'];
 $clave_validar = $_POST['clave_validar'];
-
+try {
 $sentencia = $pdo->prepare("INSERT INTO t_usuarios (nombres, email, clave_usuario, fecha_hora_creacion) 
 VALUES (:nombres, :email, :clave_usuario, :fecha_hora_creacion)");
 
@@ -15,3 +15,9 @@ $sentencia->bindParam(':clave_usuario', $clave_usuario);
 $sentencia->bindParam(':fecha_hora_creacion', $fecha_hora);
 
 $sentencia->execute();
+
+header('Location: ' . $URL . '/usuarios/index.php');
+}catch (PDOException $e) { 
+    // Detener el script si hay un error en la consulta
+    echo "Error al guardar el usuario: " . $e->getMessage();
+}
